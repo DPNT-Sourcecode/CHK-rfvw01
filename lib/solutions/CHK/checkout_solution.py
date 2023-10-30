6f973048432acbbe129ac4e2b734285fa1f0589c
sbx_user1051 <sbx_user1051@169.254.154.73>
2023-10-30 20:14:41 +0000

    Mon Oct 30 20:14:41 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,38 +3,69 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
+#     +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
+#special offer (quantity of item, price/another item?)
     price_table_and_offers = {
         "A": {"price": 50, "special_offers": (3, 130)},
         "B": {"price": 30, "special_offers": (2, 45)},
         "C": {"price": 20},
-        "D": {"price": 15}
+        "D": {"price": 15},
+        "E": {"price": 40, "special_offers": (2, "B")}
     }
 
     total_checkout_cost = 0
-    quantity_count = {}
+    # quantity_count = {}
+    special_offer_items = {}
+
+    # for item in skus:
+    #     if item in price_table_and_offers:
+    #         if item in quantity_count:
+    #             quantity_count[item] += 1
+    #         else:
+    #             quantity_count[item] = 1
+    #     else:
+    #         return -1
 
     for item in skus:
-        if item in price_table_and_offers:
-            if item in quantity_count:
-                quantity_count[item] += 1
-            else:
-                quantity_count[item] = 1
-        else:
-            return -1
-
-    for item, quantity in quantity_count.items():
         item_info = price_table_and_offers[item]
         item_price = item_info["price"]
 
         if "special_offers" in item_info:
-            special_quant, special_price = item_info["special_offers"]
-            special_count = quantity // special_quant
-            special_remainder_count = quantity % special_quant
+            special_quant, special_deal = item_info["special_offers"]
+            if special_quant == 2 and special_deal == "B":
+                special_offer_items["E"] = special_offer_items.get("E", 0)+1
+            else:
+                special_quant, special_deal = item_info["special_offers"]
+                special_offer_items[item] = special_offer_items.get(item, 0)+1
 
-            total_checkout_cost += special_count * special_price
-            total_checkout_cost += special_remainder_count * item_price
+
+            # special_count = quantity // special_quant
+            # special_remainder_count = quantity % special_quant
+
+            # total_checkout_cost += special_count * special_price
+            # total_checkout_cost += special_remainder_count * item_price
         
         else:
-            total_checkout_cost += quantity * item_price
+            total_checkout_cost += item_price
+
+    for item, quantity in special_offer_items.items():
+        item_info = price_table_and_offers[item]
+        special_quant, special_deal = item_info["special_offers"]
+        if special_quant == 2 and special_deal == "B":
+            item_price = item_info["price"]
+            b_count = special_offer_items.get("B", 0)
+            e_count = quantity // 2
+            free_bs = min(b_count,e_count)
+            total_checkout_cost += (quantity - free_bs) * item_price
+
     
     return total_checkout_cost
