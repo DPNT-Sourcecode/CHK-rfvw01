a1f3f7c7727565e84ff9d8063b9129344a7344cf
sbx_user1051 <sbx_user1051@169.254.154.73>
2023-10-30 19:00:38 +0000

    Mon Oct 30 19:00:38 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,37 +3,38 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
-    # price_table_and_offers = {
-    #     "A": {"price": 50, "special_offers": (3, 130)},
-    #     "B": {"price": 30, "special_offers": (2, 45)},
-    #     "C": {"price": 20},
-    #     "D": {"price": 15}
-    # }
+    price_table_and_offers = {
+        "A": {"price": 50, "special_offers": (3, 130)},
+        "B": {"price": 30, "special_offers": (2, 45)},
+        "C": {"price": 20},
+        "D": {"price": 15}
+    }
 
-    # total_checkout_cost = 0
-    # quantity = {}
+    total_checkout_cost = 0
+    quantity = {}
 
-    # for item, quantity in skus.items():
-    #     if item in price_table_and_offers:
-    #         item_info = price_table_and_offers[item]
-    #         item_price = item_info["price"]
+    for item in skus:
+        if item in price_table_and_offers:
+            if item in quantity:
+                quantity[item] += 1
+            else:
+                quantity[item] = 1
+        else:
+            return -1
 
-    #         if "special"
+    for item, quantity in quantity.items():
+        item_info = price_table_and_offers[item]
+        item_price = item_info["price"]
 
+        if "special_offers" in item_info:
+            special_quant, special_price = item_info["special_offers"]
+            special_count = quantity // special_quant
+            special_remainder_count = quantity % special_quant
 
-
-
-
-
-
-
-
-# define the pricings in dict
-#loop through the skus - thing its just the items [A, B,A,C,D]
-#if item is in the dict : - get the price
-#  
-# if item in dict AND theres a special offer - return the quantity and price - figure out if quantity is big enough for special offer (%)
-#Add special offer price to total checkout cost and normal price on remainder
-# Add items that doen't have special offers to the total 
-#If item in cart isn't in the dict return -1
+            total_checkout_cost += special_count * special_price
+            total_checkout_cost += special_remainder_count * item_price
+        
+        else:
+            total_checkout_cost += quantity * item_price
+    
+    return total_checkout_cost
