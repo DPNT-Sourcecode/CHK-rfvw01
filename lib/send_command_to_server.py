aec16cd164eea739c07bd58fa62fbe562035a962
sbx_user1051 <sbx_user1051@169.254.154.73>
2023-10-30 17:22:12 +0000

    Mon Oct 30 17:22:12 UTC 2023

--- lib/send_command_to_server.py
+++ lib/send_command_to_server.py
@@ -34,7 +34,7 @@ from runner.user_input_action import get_user_input
  
     +------+-----------------------------------------------------------------------+
     | Step | The usual workflow                                                    |
-    +------+-----------------------------------------------------------------------+deploy
+    +------+-----------------------------------------------------------------------+
     |  1.  | Run this file.                                                        |
     |  2.  | Start a challenge by typing "start".                                  |
     |  3.  | Read the description from the "challenges" folder.                    |
@@ -71,3 +71,4 @@ ChallengeSession\
     .with_config(Utils.get_config())\
     .with_action_provider(lambda: get_user_input(sys.argv[1:]))\
     .start()
+
